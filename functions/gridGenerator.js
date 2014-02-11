6f319e488e4a9a1dcf35cd45a0345c4c6ca3a440
KevOnWeb <nazzari.kevin@gmail.com>
2014-02-11 15:45:26 +0100

    Ajout du modèle de grille & de son generateur graphique

---
+++ functions/gridGenerator.js
@@ -0,0 +1,28 @@
+function gridGenerator(grid, context)
+{	
+    context.fillStyle = "#000000"; // bg color
+    context.fillRect(0, 0, grid.width, grid.height);
+
+    // grid generation
+    for(y=0;y<grid.linesNumber;y++) // for each lines
+    {
+    	for (x=0;x<grid.columnsNumber;x++) // for each columns
+    	{
+    		if (grid.tab[y][x] == 1)
+    		{
+    			context.fillStyle = "#0B3FD9"; // bg color
+    			context.fillRect(grid.squareSize*x,grid.squareSize*y,grid.squareSize,grid.squareSize); // draw a wall
+    		} else if (grid.tab[y][x] == 2)
+    		{
+    			context.fillStyle = "#000000"; // bg color
+    			context.fillRect(grid.squareSize*x,grid.squareSize*y,grid.squareSize,grid.squareSize); // draw a road
+    			context.fillStyle = "#454545"; // bg color
+    			context.fillRect((grid.squareSize*x)+((grid.squareSize-grid.littleFoodSize)/2),(grid.squareSize*y)+((grid.squareSize-grid.littleFoodSize)/2),grid.littleFoodSize,grid.littleFoodSize); // draw a little food
+    		}  else
+    		{
+    			context.fillStyle = "#000000"; // bg color
+    			context.fillRect(grid.squareSize*x,grid.squareSize*y,grid.squareSize,grid.squareSize); // draw a road
+    		}
+    	}
+    }
+}
