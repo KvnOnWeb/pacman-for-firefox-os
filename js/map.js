b3455bad0b6a75ddd0e2f23a93ad30c6d8addf94
TeliHub <grayfox91@hotmail.fr>
2014-04-29 01:59:41 +0200

    Change map for food & fruit drawing

--- js/map.js
+++ js/map.js
@@ -3,24 +3,28 @@ var map = {
     columns: 19,
     squareSize: 16,
     littleFoodSize: 1,
+    bigFoodSize: 4,
+    end: 191,
+    level: 1,
+    fruitImage: new Image(),
     grid: new Array(
         new Array(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
         new Array(1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1),
         new Array(1, 2, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1, 2, 1),
-        new Array(1, 2, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1, 2, 1),
+        new Array(1, 4, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1, 4, 1),
         new Array(1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1),
         new Array(1, 2, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 2, 1),
         new Array(1, 2, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 2, 1),
         new Array(1, 1, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1, 1, 1),
-        new Array(2, 2, 2, 1, 2, 1, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1, 2, 2, 2),
-        new Array(1, 1, 1, 1, 2, 1, 2, 1, 1, 2, 1, 1, 2, 1, 2, 1, 1, 1, 1),
-        new Array(2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2),
+        new Array(0, 0, 0, 1, 2, 1, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1, 0, 0, 0),
+        new Array(1, 1, 1, 1, 2, 1, 2, 1, 1, 5, 1, 1, 2, 1, 2, 1, 1, 1, 1),
+        new Array(2, 2, 2, 2, 2, 2, 2, 1, 0, 0, 0, 1, 2, 2, 2, 2, 2, 2, 2),
         new Array(1, 1, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 1, 1),
-        new Array(2, 2, 2, 1, 2, 1, 2, 2, 2, 7, 2, 2, 2, 1, 2, 1, 2, 2, 2),
+        new Array(0, 0, 0, 1, 2, 1, 2, 2, 2, 2, 2, 2, 7, 1, 2, 1, 0, 0, 0),
         new Array(1, 1, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 1, 1),
         new Array(1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1),
         new Array(1, 2, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1, 2, 1),
-        new Array(1, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 1),
+        new Array(1, 4, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 4, 1),
         new Array(1, 1, 2, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 2, 1, 1),
         new Array(1, 2, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 2, 1),
         new Array(1, 2, 1, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 1, 2, 1),
@@ -40,22 +44,30 @@ var map = {
                 // Little Food
                     context.fillStyle = "#000000"; // bg color
                     context.fillRect(this.squareSize*x, this.squareSize*y,this.squareSize,this.squareSize); // draw a road
-                    context.fillStyle = "#454545"; // bg color
+                    context.fillStyle = "#FFF"; // bg color
                     context.fillRect((this.squareSize*x)+((this.squareSize-this.littleFoodSize)/2), (this.squareSize*y)+((this.squareSize-this.littleFoodSize)/2),this.littleFoodSize,this.littleFoodSize); // draw a little food
-                }  
-                else if (this.grid[y][x] == 7) {
+                }  else if (this.grid[y][x] == 4) {
+                // Little Food
+                    context.fillStyle = "#000000"; // bg color
+                    context.fillRect(this.squareSize*x, this.squareSize*y,this.squareSize,this.squareSize); // draw a road
+                    context.fillStyle = "#FFF"; // bg color
+                    context.fillRect((this.squareSize*x)+((this.squareSize-this.bigFoodSize)/2), (this.squareSize*y)+((this.squareSize-this.bigFoodSize)/2),this.bigFoodSize,this.bigFoodSize); // draw a little food
+                }  else if (this.grid[y][x] == 5) {
+                // Little Food
+                    context.fillStyle = "#000000"; // bg color
+                    context.fillRect(this.squareSize*x, this.squareSize*y,this.squareSize,this.squareSize); // draw a road
+                    context.fillStyle = "#FFF"; // bg color
+                    context.fillRect((this.squareSize*x), (this.squareSize*y)+(this.squareSize/2)-1,this.squareSize,2); // draw a little food
+                } else if (this.grid[y][x] == 7) {
                 //  Fruit
                     context.fillStyle = "#000000"; // bg color
                     context.fillRect(this.squareSize*x, this.squareSize*y,this.squareSize,this.squareSize);
-                    var imgFruit = new Image();
-                    imgFruit.src = '../img/fraise.png'; 
-                    imgFruit.onload = function(){
-                        var pattern = context.createPattern(this, "repeat");
-                        context.fillStyle = pattern;
-                        context.fill();
+                    
+                    if (this.level == 1) {
+                        this.fruitImage.src = 'img/cherry.png'; 
                     }
-                }
-                else {
+                    context.drawImage(this.fruitImage, (this.squareSize*x), (this.squareSize*y), 16, 16);                    
+                } else {
                 // Road
                     context.fillStyle = "#000000"; // bg color
                     context.fillRect(this.squareSize*x, this.squareSize*y,this.squareSize,this.squareSize); // draw a road
